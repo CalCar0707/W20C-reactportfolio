3122a9fd6c1974b0ae7b89b780402c2ea132c1d6
Call Carl <callienicole0419@gmail.com>
2023-03-23 18:51:14 -0500

    updated footer.js and imported

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div>
+        <h4>Github: https://github.com/CalCar0707</h4>
+        <h4>LinkedIn: https://www.linkedin.com/in/callie-baliles/</h4>
+        <h4>***ADD A THIRD PLATFROM LINK***</h4>
+        </div>
+    )
+}
--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Footer from './Footer';
 
 export default function WelcomeHeader() {
     return (
         <header>
             <h1>Callie Baliles</h1>
             <Navigation />
+            <Footer />
         </header>
     );
 };
