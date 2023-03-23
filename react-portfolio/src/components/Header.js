0ba9379ecdbe62d894e83bb97b0339c72b870d42
Call Carl <callienicole0419@gmail.com>
2023-03-23 17:25:39 -0500

    updated header.js and added to app.js

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function WelcomeHeader() {
+    return (
+        <header>
+            <h1>Callie Baliles</h1>
+            <nav>
+                <ul>
+                    <li>About Me</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                    <li>Resume</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default WelcomeHeader;
