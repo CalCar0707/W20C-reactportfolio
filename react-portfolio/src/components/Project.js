3624594a09c1a96b1dcfa672fba1aca8f66d959f
Call Carl <callienicole0419@gmail.com>
2023-03-27 21:57:46 -0500

    added more projects to portfolio section

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -6,7 +6,9 @@ import Form from "./Form";
 //import pics and screenshots
 import profilePic from './../img/portfolio pic.jpg';
 import weatherMe from './../img/weatherme-screenshot.png';
-import Food4Thought from './../img/Food4thought-screenshot.png';
+import food4Thought from './../img/Food4thought-screenshot.png';
+import scheduler from './../img/scheduler-screenshot.png';
+import weather from './../img/weatherdash-screenshot.jpg';
 
 
 export default function Project(){
@@ -20,13 +22,38 @@ export default function Project(){
             </div>
             <div id="portfolio">
                 <h2>Portfolio</h2>
+                <h3>WeatherMe</h3>
                 <a href="https://bravenbright.github.io/WeatherMe/#gsc.tab=0">
-                    <h3>WeatherMe</h3></a>
+                    Deployed</a>
+                    <a href="https://github.com/bravenbright/WeatherMe">
+                    Repository
+                    </a>
                     <img src={weatherMe} className="projectpic" />
-                <a href="https://f4t-db.herokuapp.com/">
                     <h3>Food4Thought</h3>
-                    <img src={Food4Thought} className="projectpic" />
-                </a>
+                    <a href="https://f4t-db.herokuapp.com/">
+                        Deployed
+                    </a>
+                    <a href="https://github.com/DaniaOn/food-4-thought">
+                        Repository
+                    </a>
+                    <img src={food4Thought} className="projectpic" />
+                 <h3>Weather Dashboard</h3>
+                    <a href="https://calcar0707.github.io/W6C-weather/">
+                        Deployed</a>
+                    <a href="https://github.com/CalCar0707/W6C-weather">
+                        Repository
+                    </a>
+                    <img src={weather} className="projectpic" />
+                    <h3>Workday Scheduler</h3>
+                    <a href="https://calcar0707.github.io/W5C-scheduler/">
+                        Deployed
+                    </a>
+                    <a href="https://github.com/CalCar0707/W5C-scheduler">
+                        Repository
+                    </a>
+                    <img src={scheduler} className="projectpic" />
+
+                
                 
 
             </div>
