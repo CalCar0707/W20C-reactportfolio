28186ed1f12d407d5e16b8e67c35265d530e12d4
Call Carl <callienicole0419@gmail.com>
2023-04-17 20:08:31 -0500

    added movie snackers to portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,6 +9,7 @@ import weatherMe from '../img/weatherme-screenshot.png';
 import food4Thought from '../img/Food4thought-screenshot.png';
 import scheduler from '../img/scheduler-screenshot.png';
 import weather from '../img/weatherdash-screenshot.png';
+import movieSnackers from '../img/moviesnackersscreenshot.png';
 
 
 export default function Project(){
@@ -22,13 +23,20 @@ export default function Project(){
             </div>
             <div id="portfolio">
                 <h2>Portfolio</h2>
+                <h3>Movie Snackers</h3>
+                <a href="https://movie-snackers.herokuapp.com/">
+                    Deployed
+                </a><a href="https://github.com/willj30/review-blog">
+                    Repository
+                </a>
+                <img src={movieSnackers} className="projectpic" alt=" "/>
                 <h3>WeatherMe</h3>
                 <a href="https://bravenbright.github.io/WeatherMe/#gsc.tab=0">
                     Deployed</a>
                     <a href="https://github.com/bravenbright/WeatherMe">
                     Repository
                     </a>
-                    <img src={weatherMe} className="projectpic" />
+                    <img src={weatherMe} className="projectpic" alt=" "/>
                     <h3>Food4Thought</h3>
                     <a href="https://f4t-db.herokuapp.com/">
                         Deployed
@@ -36,14 +44,14 @@ export default function Project(){
                     <a href="https://github.com/DaniaOn/food-4-thought">
                         Repository
                     </a>
-                    <img src={food4Thought} className="projectpic" />
+                    <img src={food4Thought} className="projectpic" alt=""/>
                  <h3>Weather Dashboard</h3>
                     <a href="https://calcar0707.github.io/W6C-weather/">
                         Deployed</a>
                     <a href="https://github.com/CalCar0707/W6C-weather">
                         Repository
                     </a>
-                    <img src={weather} className="projectpic" />
+                    <img src={weather} className="projectpic" alt=""/>
                     <h3>Workday Scheduler</h3>
                     <a href="https://calcar0707.github.io/W5C-scheduler/">
                         Deployed
@@ -51,7 +59,7 @@ export default function Project(){
                     <a href="https://github.com/CalCar0707/W5C-scheduler">
                         Repository
                     </a>
-                    <img src={scheduler} className="projectpic" />
+                    <img src={scheduler} className="projectpic" alt=""/>
 
 
                 
