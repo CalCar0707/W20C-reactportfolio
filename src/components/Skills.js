728fa23669d49278e14a2ee3f1a2508743ccc2e6
CalCarl <callienicole0419@gmail.com>
2023-07-27 12:31:28 -0500

    added skills component and logos

---
+++ src/components/Skills.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//import logos
+import HTML from '../img/html.png';
+import CSS from '../img/css.png';
+import JavaScript from '../img/javascript.png';
+import ReactImg from '../img/react.png';
+import Node from '../img/node.png';
+import GitHub from '../img/github.png';
+import Mongo from '../img/mongo.png';
+import Tailwind from '../img/tailwind.png';
+
+export default function Skills() {
+    return (
+        <div>
+
+        </div>
+    )
+}
