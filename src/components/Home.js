85696b513a5b26c2310ba84c8dec99229d77add8
CalCarl <callienicole0419@gmail.com>
2023-09-21 13:52:28 -0500

    removed irrelevant comments, added functionality to view work button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {HiArrowNarrowRight} from 'react-icons/hi';
 import Logo from '../img/CB-sig-2.1.png';
-// import Background from '../img/bg-1.jpg';
-// import Form from "./Form";
-
+import {Link} from 'react-scroll';
 
 import profilePic from '../img/portfolio pic.jpg';
 
@@ -22,64 +20,12 @@ export default function Home(){
                 <h2>I'm a Full Stack Developer.</h2>
                 {/* <p>small about me section</p> */}
                 <div>
-                    <button className='border-2 px-6 py-3 my-2 flex items-center'>View Work <HiArrowNarrowRight className='ml-3' /></button>
+                    <button className='border-2 px-6 py-3 my-2 flex items-center'>
+                        <Link to="work">View Work </Link><HiArrowNarrowRight className='ml-3' /></button>
                 </div>
                 </div>
             </div>
-            {/* <div id="portfolio">
-                <h2>Portfolio</h2>
-                <h3>Movie Snackers</h3>
-                <a href="https://movie-snackers.herokuapp.com/">
-                    Deployed
-                </a><a href="https://github.com/willj30/review-blog">
-                    Repository
-                </a>
-                <img src={movieSnackers} className="projectpic" alt=" "/>
-                <h3>WeatherMe</h3>
-                <a href="https://bravenbright.github.io/WeatherMe/#gsc.tab=0">
-                    Deployed</a>
-                    <a href="https://github.com/bravenbright/WeatherMe">
-                    Repository
-                    </a>
-                    <img src={weatherMe} className="projectpic" alt=" "/>
-                    <h3>Food4Thought</h3>
-                    <a href="https://f4t-db.herokuapp.com/">
-                        Deployed
-                    </a>
-                    <a href="https://github.com/DaniaOn/food-4-thought">
-                        Repository
-                    </a>
-                    <img src={food4Thought} className="projectpic" alt=""/>
-                 <h3>Weather Dashboard</h3>
-                    <a href="https://calcar0707.github.io/W6C-weather/">
-                        Deployed</a>
-                    <a href="https://github.com/CalCar0707/W6C-weather">
-                        Repository
-                    </a>
-                    <img src={weather} className="projectpic" alt=""/>
-                    <h3>Workday Scheduler</h3>
-                    <a href="https://calcar0707.github.io/W5C-scheduler/">
-                        Deployed
-                    </a>
-                    <a href="https://github.com/CalCar0707/W5C-scheduler">
-                        Repository
-                    </a>
-                    <img src={scheduler} className="projectpic" alt=""/>
- */}
 
-                
-
-            {/* </div>
-            <div id="contact-form">
-                <Form />
-
-            </div> */}
-            {/* <div id="resume">
-                <h2>Resume</h2>
-            </div>
-            {/* <div className="footer">
-                <Footer />
-            </div> */} 
         </div>
     );
 };
