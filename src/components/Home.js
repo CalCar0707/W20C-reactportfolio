f7b5f06d6bba69729a57b782cc5f4ccc6a6f5d63
CalCarl <callienicole0419@gmail.com>
2023-09-25 12:46:40 -0500

    updated home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,16 +14,14 @@ export default function Home(){
             <div className='max-w-[1000px] mx-auto px-8 pt-[200px] flex flex-row justify-center content-center'>
                 <img src={profilePic} className='profilepic pr-10 ' alt='developer pic' />
                 <div className='flex flex-col justify-center '>
-                <p>Hello, my name is</p>
+                <p className='text-3xl'>Hello, my name is</p>
                 {/* <img className='h-[80px] py-[8px]' src={Logo} /> */}
-                <h1 className='text-4xl sm:text-7xl'>Callie Baliles</h1>
-                <h2>I'm a Full Stack Developer.</h2>
-                <p>I graduated the University of Texas at Austin Full Stack Web Development Bootcamp. During my education, 
-                I learned the languages and skills required to build a fully functional website from the front-end to the back-end. Utilizing
-                all  sorts of languages and applications from HTML, CSS, JavaScript and  React to node.js, mongodb, express.js, and more. Please
-                take a look around.</p>
+                <h1 className='text-5xl sm:text-7xl font-bold'>Callie Baliles</h1>
+                <h2 className='text-3xl'>I'm a Full Stack Developer.</h2>
+                <p className='text-2xl pt-1'>   I graduated the University of Texas at Austin Full Stack Web Development Bootcamp. During my education, 
+                I learned the languages and skills required to build a fully functional website from the front-end to the back-end.</p>
                 <div>
-                    <button className='border-2 px-6 py-3 my-2 flex items-center'>
+                    <button className='text-2xl border-2 px-6 py-3 my-2 flex items-center'>
                         <Link to="work">View Work </Link><HiArrowNarrowRight className='ml-3' /></button>
                 </div>
                 </div>
