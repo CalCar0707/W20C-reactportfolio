be6fe644d0a63f18e07a9c9b4ca8c33c4688f012
CalCarl <callienicole0419@gmail.com>
2023-09-29 12:19:36 -0500

    fixed text sizing in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,8 @@ export default function Home(){
                 <div className='flex flex-col justify-center '>
                 <p className='text-3xl'>Hello, my name is</p>
         
-                <h1 className='text-7xl sm:text-5xl font-bold'>Callie Baliles</h1>
-                <h2 className='text-3xl sm:text-5xl'>I'm a Full Stack Web Developer.</h2>
+                <h1 className='text-5xl sm:text-7xl font-bold'>Callie Baliles</h1>
+                <h2 className='text-3xl '>I'm a Full Stack Web Developer.</h2>
                 <p className='text-1xl py-4 max-w-[700px]'>   I graduated the University of Texas at Austin Full Stack Web Development Bootcamp. During my education, 
                 I learned the languages and skills required to build a fully functional web application. I am passionate about 
                 providing a great user experience from front-end to back-end, utilizing best practices.</p>
