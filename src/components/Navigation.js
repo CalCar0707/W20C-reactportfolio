f769e32fd6f5c7438edbf3142ee8f5f451196ce7
CalCarl <callienicole0419@gmail.com>
2023-09-29 08:52:38 -0500

    added border and padding to mobile nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,7 @@ export default function Navigation() {
 
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-[#f7f7f7]'>
-                <img src={Logo} alt='Logo Image' style={{width: '225px'}} className='pl-[30px] pt-1'/>
+                <img src={Logo} alt='Logo Image' style={{width: '225px'}} className='pl-[30px] pt-1 sm:pl-[10px]'/>
            {/* menu */}
            <div>
                 <ul className='hidden md:flex text-2xl pr-[30px]'>
@@ -56,31 +56,31 @@ export default function Navigation() {
             {/* mobile menu */}
             <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] text-[#f7f7f7] flex flex-col justify-center items-center'}>
             <ul>
-                        <li className='py-6 text-3xl'>
+                        <li className='py-6 pl-[30px] text-3xl border-b-2 border-[#f7f7f7]'>
                         <Link onClick={handleClick} to="home" smooth={true} duration={500} >
                             Home
                         </Link>
                         </li>
                         
-                        <li className='py-6 text-3xl'>
+                        <li className='py-6 pl-[30px] text-3xl border-b-2 border-[#f7f7f7]'>
                         <Link onClick={handleClick} to="about" smooth={true} duration={500} >
                             About
                         </Link>
                         </li>
                         
-                        <li className='py-6 text-3xl'>
+                        <li className='py-6 pl-[30px] text-3xl border-b-2 border-[#f7f7f7]'>
                         <Link onClick={handleClick} to="skills" smooth={true} duration={500} >
                             Skills
                         </Link>
                         </li>
                         
-                        <li className='py-6 text-3xl'>
+                        <li className='py-6 pl-[30px] text-3xl border-b-2 border-[#f7f7f7]'>
                         <Link onClick={handleClick} to="work" smooth={true} duration={500} >
                             Work
                         </Link>
                         </li>
                     
-                        <li className='py-6 text-3xl'>
+                        <li className='py-6 pl-[15px] text-3xl '>
                         <Link onClick={handleClick} to="contact" smooth={true} duration={500} >
                             Contact
                         </Link>
